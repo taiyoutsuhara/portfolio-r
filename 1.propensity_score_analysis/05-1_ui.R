62ec4ae26290905c7b423c62cd60ebc987579e28
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2020-01-15 00:01:26 +0900

    rename: 比較したいサービスを選択するタブパネルのファイル名

--- 1.propensity_score_analysis/05-1_ui.R
+++ 1.propensity_score_analysis/05-1_ui.R
@@ -2,7 +2,7 @@
 
 # 各種パーツの読込み #
 source(paste0(getwd(), "/05-1_ui_info.R"), encoding = "UTF-8")
-source(paste0(getwd(), "/05-1_ui_tabPanel_at_comparison.R"), encoding = "UTF-8")
+source(paste0(getwd(), "/05-1_ui_tabPanel_of_comparison.R"), encoding = "UTF-8")
 source(paste0(getwd(), "/05-1_ui_comparison.R"), encoding = "UTF-8")
 source(paste0(getwd(), "/05-1_ui_ranking.R"), encoding = "UTF-8")
 source(paste0(getwd(), "/05-1_ui_to_render.R"), encoding = "UTF-8")
